f5662b81172565c279f8a0b521e0d409a878b872
Mihuy1 <s2000954@edu.bc.fi>
2022-09-05 13:56:24 +0300

    Lisätty projekti ja unit class

---
+++ Peli/Unit.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Peli
+{
+    public class Unit
+    {
+        public string playerName;
+        public int damage;
+        public int health;
+
+        public Unit(string playerName, int damage, int health)
+        {
+            this.playerName = playerName;
+            this.damage = damage;
+            this.health = health;
+        }
+    }
+}
