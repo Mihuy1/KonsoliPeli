05ab0c6161f888654616bb522aa1c7b8e8311c1e
Mihuy1 <s2000954@edu.bc.fi>
2022-09-15 09:56:40 +0300

    Lisätty kerättäviä boxeja jne

--- Peli/Program.cs
+++ Peli/Program.cs
@@ -1,1 +1,24 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Peli
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Random rnd = new Random();
+
+            Unit unit = new Unit("testi", "testi2", 2, 2, 50, 50);
+
+            unit.Fight();
+
+           
+        }
+    }
+}
--- Peli/Unit.cs
+++ Peli/Unit.cs
@@ -3,74 +3,115 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace Peli
 {
     public class Unit
     {
-        public string playerName;
-        public string playerName2;
-        public int damage;
-        public int damage2;
-        public int health;
-        public int health2;
+        public string humanWarrior;
+        public int humanWarriorHP;
+        public int humanWarriorDMG;
 
-        public Unit(string playerName, string playerName2, int damage, int damage2, int health, int health2)
+        public string humanArcher;
+        public int humanArcherHP;
+        public int humanArcherDMG;
+
+        public string humanMage;
+        public string humanMageHP;
+        public string humanMageDMG;
+
+        public string skeletonWarrior;
+        public int skeletonWarriorHP;
+        public int skeletonWarriorDMG;
+
+        public Unit(string humanWarrior, string skeletonWarrior, int humanWarriorDMG, int skeletonWarriorHP, int humanWarriorHP, int skeletonWarriorDMG)
         {
-            this.playerName = playerName;
-            this.playerName2 = playerName2;
-            this.damage = damage;
-            this.damage2 = damage2;
-            this.health = health;
-            this.health2 = health2;
+            this.humanWarrior = humanWarrior;
+            this.skeletonWarrior = skeletonWarrior;
+            this.humanWarriorDMG = humanWarriorDMG;
+            this.skeletonWarriorDMG = skeletonWarriorDMG;
+            this.humanWarriorHP = humanWarriorHP;
+            this.skeletonWarriorHP = skeletonWarriorHP;
         }
 
         public void Fight()
         {
             Random random = new Random();
 
-            playerName = "Human Warrior";
-            playerName2 = "Skeleton Warrior";
-            health = random.Next(25, 100);
-            health2 = random.Next(25, 100);
+            humanWarrior = "Human Warrior";
+            skeletonWarrior = "Skeleton Warrior";
+            humanWarriorHP = random.Next(50, 100);
+            skeletonWarriorHP = random.Next(50, 100);
+
+            Console.WriteLine(humanWarrior + " has " + humanWarriorHP + " health" + " and " + humanWarriorDMG + " damage");
+            Console.WriteLine(skeletonWarrior + " has " + skeletonWarriorHP + " health" + " and " + humanWarriorDMG + " damage");
+
+            Console.WriteLine("To start press enter...");
+            Console.ReadLine();
+
+            Console.WriteLine("Test");
 
             Damage();
-            
+
         }
 
         public void Damage(bool firstPlayerAttacked = false, bool secondPlayerAttacked = false)
         {
-            while(health > 0 || health2 > 0)
+
+            while (humanWarriorHP >= 0 && skeletonWarriorHP >= 0)
             {
                 Random rand = new Random();
 
+
                 if (firstPlayerAttacked == false)
                 {
-                    for (int i = 0; i < health; i++)
-                    {
-                        damage = rand.Next(5, 20);
 
-                        health -= damage2;
+                    for (int i = 0; i < 1; i++)
+                    {
+                        if (humanWarriorHP <= 0)
+                        {
+                            Console.WriteLine(humanWarrior + " died!");
+                        }
+
+                        humanWarriorDMG = rand.Next(5, 20); // Set random damage
+
+                        skeletonWarriorHP -= humanWarriorDMG; // Second player takes damage from first player.
                         firstPlayerAttacked = true;
                         secondPlayerAttacked = false;
 
-                        Console.WriteLine(playerName + " attacked " + playerName2 + " and dealt " + damage + " of damage");
-                        Console.WriteLine(playerName +" has" + health + " health and " + playerName2 + " has" + health2 + " health");;
+                        Console.WriteLine(humanWarrior+ " attacked " + skeletonWarrior + " and dealt " + humanWarriorDMG + " of damage");
+                        Console.WriteLine(humanWarrior + " has " + humanWarriorHP + " health and " + skeletonWarrior + " has" + skeletonWarriorHP + " health");
+
+                        // To continue user has to press a key.
+                        Console.WriteLine("Press Enter to continue....");
+
+                        Console.ReadLine();
                     }
                 }
 
-                if (secondPlayerAttacked == false)
+                else if (secondPlayerAttacked == false)
                 {
-                    for (int i = 0; i < health;i++)
-                    {
-                        damage2 = rand.Next(5, 20);
 
-                        health2 -= damage;
+                    if (skeletonWarriorHP <= 0)
+                    {
+                        Console.WriteLine(skeletonWarrior + " died");
+                    }
+
+                    for (int i = 0; i < 1;i++)
+                    {
+                        skeletonWarriorDMG = rand.Next(5, 20); // set random damage
+
+                        humanWarriorHP -= skeletonWarriorDMG; // First player takes damage from second player.
                         secondPlayerAttacked = true;
                         firstPlayerAttacked = false;
 
-                        Console.WriteLine(playerName2 + " attacker " + playerName + " and dealth" + damage + " of damage");
-                        Console.WriteLine(playerName + " has" + health + " health and " + playerName2 + " has" + health2 + " health");
+                        Console.WriteLine(skeletonWarrior + " attacked " + humanWarrior + " and dealt " + skeletonWarriorDMG + " of damage");
+                        Console.WriteLine(skeletonWarrior + " has " + skeletonWarriorHP + " health and " + humanWarrior + " has " + humanWarriorHP + " health");
+
+                        Console.WriteLine("Press Enter to continue....");
+
+                        Console.ReadLine();
                     }
                 }
             }
