bbcb7b803b06318310a19083931d4dadaa20a50a
Mihuy1 <s2000954@edu.bc.fi>
2022-09-19 13:47:09 +0300

    AI ja pelaajan joukon koodia lisää

--- Peli/Unit.cs
+++ Peli/Unit.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
@@ -65,12 +66,14 @@ namespace Peli
             skeletonArcherHP = 60;
             skeletonMageHP = 50;
 
+            // List armies
             Console.WriteLine("Player's army: ");
-            Console.WriteLine(humanWarrior + "\n" + humanArcher + "\n" + humanMage);
+            Console.WriteLine("1: " + humanWarrior + "\n" + "2: " + humanArcher + "\n" + "3: " + humanMage);
 
             Console.WriteLine("AI's army: ");
-            Console.WriteLine(skeletonWarrior + "\n" + skeletonArcher + "\n" + skeletonMage);
+            Console.WriteLine("1: " + skeletonWarrior + "\n" + "2: " + skeletonArcher + "\n" + "3: " + skeletonMage);
 
+            // Press enter to start game...
             Console.WriteLine("To start press enter...");
             Console.ReadLine();
 
@@ -81,30 +84,72 @@ namespace Peli
         public void Damage(bool firstPlayerAttacked = false, bool secondPlayerAttacked = false)
         {
 
-            while (humanWarriorHP >= 0 && humanArcherHP >= 0 && humanMageHP >= 0 && skeletonWarriorHP >= 0 && skeletonArcherHP >= 0
-                && skeletonMageHP >= 0)
+            int playerArmy = humanWarriorHP + humanArcherHP + humanMageHP;
+            int aiArmy = skeletonWarriorHP + skeletonArcherHP + skeletonMageHP;
+
+            while (playerArmy >= 0 && aiArmy >= 0)
             {
                 Random rand = new Random();
 
 
                 if (firstPlayerAttacked == false)
                 {
+                    Console.WriteLine("Give number to choose: ");
+                    int player = Convert.ToInt32(Console.ReadLine());
+                    string chosenPlayer;
+                    int chosenPlayerDMG;
+
+                    if (player == 1)
+                    {
+                        chosenPlayer = humanWarrior;
+                        chosenPlayerDMG = humanWarriorDMG;
+                        player = humanWarriorHP;
+                    }
+                    else if (player == 2)
+                    {
+                        chosenPlayer = humanArcher;
+                        chosenPlayerDMG = humanArcherDMG;
+                        player = humanArcherHP;
+                    }
+                    else
+                    {
+                        chosenPlayer = humanMage;
+                        chosenPlayerDMG = humanMageDMG;
+                        player = humanMageHP;
+                    }
+
+                    Console.WriteLine("Choose target: ");
+                    int target = Convert.ToInt32(Console.ReadLine());
+                    string chosenTarget;
+
+                    if (target == 1 && skeletonWarriorHP > 0)
+                    {
+                        target = skeletonWarriorHP;
+                        chosenTarget = skeletonWarrior;
+                    } else if (target == 2 && skeletonArcherHP > 0)
+                    {
+                        target = skeletonArcherHP;
+                        chosenTarget = skeletonArcher;
+                    } else if (target == 3 && skeletonMageHP > 0)
+                    {
+                        target = skeletonMageHP;
+                        chosenTarget = skeletonMage;
+                    }
 
                     for (int i = 0; i < 1; i++)
                     {
-                        if (humanWarriorHP <= 0)
+                        if (player <= 0)
                         {
-                            Console.WriteLine(humanWarrior + " died!");
+                            Console.WriteLine(chosenPlayer + " died!");
                         }
 
-                        humanWarriorDMG = rand.Next(5, 20); // Set random damage
+                        chosenPlayerDMG = rand.Next(5, 20); // Set random damage for chosen character
 
-                        skeletonWarriorHP -= humanWarriorDMG; // Second player takes damage from first player.
-                        firstPlayerAttacked = true;
-                        secondPlayerAttacked = false;
+                        target -= chosenPlayerDMG; // AI takes damage from player
+                        firstPlayerAttacked = true; // Player's army has attacked
+                        secondPlayerAttacked = false; // AI's army hasn't attacked yet
 
-                        Console.WriteLine(humanWarrior + " attacked " + skeletonWarrior + " and dealt " + humanWarriorDMG + " of damage");
-                        Console.WriteLine(humanWarrior + " has " + humanWarriorHP + " health and " + skeletonWarrior + " has " + skeletonWarriorHP + " health");
+                        // Print dealth damage & health here:
 
                         // To continue user has to press a key.
                         Console.WriteLine("Press Enter to continue....");
