2f7d9b55f20cdbc4b3c4fc7b56f3df9a0a2571de
Mihuy1 <s2000954@edu.bc.fi>
2022-09-19 12:47:39 +0300

    Lisätty kaksi joukkoa
    - Vihollisen joukko & pelaajan joukko

--- Peli/Unit.cs
+++ Peli/Unit.cs
@@ -18,39 +18,61 @@ namespace Peli
         public int humanArcherDMG;
 
         public string humanMage;
-        public string humanMageHP;
-        public string humanMageDMG;
+        public int humanMageHP;
+        public int humanMageDMG;
 
         public string skeletonWarrior;
         public int skeletonWarriorHP;
         public int skeletonWarriorDMG;
 
+        public string skeletonArcher;
+        public int skeletonArcherHP;
+        public int skeletonArcherDMG;
+
+        public string skeletonMage;
+        public int skeletonMageHP;
+        public int skeletonMageDMG;
+
         public Unit(string humanWarrior, string skeletonWarrior, int humanWarriorDMG, int skeletonWarriorHP, int humanWarriorHP, int skeletonWarriorDMG)
         {
             this.humanWarrior = humanWarrior;
             this.skeletonWarrior = skeletonWarrior;
+
             this.humanWarriorDMG = humanWarriorDMG;
             this.skeletonWarriorDMG = skeletonWarriorDMG;
+
             this.humanWarriorHP = humanWarriorHP;
             this.skeletonWarriorHP = skeletonWarriorHP;
         }
 
         public void Fight()
         {
-            Random random = new Random();
 
             humanWarrior = "Human Warrior";
-            skeletonWarrior = "Skeleton Warrior";
-            humanWarriorHP = random.Next(50, 100);
-            skeletonWarriorHP = random.Next(50, 100);
+            humanArcher = "Human Archer";
+            humanMage = "Human Mage";
 
-            Console.WriteLine(humanWarrior + " has " + humanWarriorHP + " health" + " and " + humanWarriorDMG + " damage");
-            Console.WriteLine(skeletonWarrior + " has " + skeletonWarriorHP + " health" + " and " + humanWarriorDMG + " damage");
+            skeletonWarrior = "Skeleton Warrior";
+            skeletonArcher = "Skeleton Archer";
+            skeletonMage = "Skeleton Mage";
+
+            humanWarriorHP = 100;
+            humanArcherHP = 60;
+            humanMageHP = 50;
+
+
+            skeletonWarriorHP = 100;
+            skeletonArcherHP = 60;
+            skeletonMageHP = 50;
+
+            Console.WriteLine("Player's army: ");
+            Console.WriteLine(humanWarrior + "\n" + humanArcher + "\n" + humanMage);
+
+            Console.WriteLine("AI's army: ");
+            Console.WriteLine(skeletonWarrior + "\n" + skeletonArcher + "\n" + skeletonMage);
 
             Console.WriteLine("To start press enter...");
             Console.ReadLine();
-
-            Console.WriteLine("Test");
 
             Damage();
 
@@ -59,7 +81,8 @@ namespace Peli
         public void Damage(bool firstPlayerAttacked = false, bool secondPlayerAttacked = false)
         {
 
-            while (humanWarriorHP >= 0 && skeletonWarriorHP >= 0)
+            while (humanWarriorHP >= 0 && humanArcherHP >= 0 && humanMageHP >= 0 && skeletonWarriorHP >= 0 && skeletonArcherHP >= 0
+                && skeletonMageHP >= 0)
             {
                 Random rand = new Random();
 
@@ -80,8 +103,8 @@ namespace Peli
                         firstPlayerAttacked = true;
                         secondPlayerAttacked = false;
 
-                        Console.WriteLine(humanWarrior+ " attacked " + skeletonWarrior + " and dealt " + humanWarriorDMG + " of damage");
-                        Console.WriteLine(humanWarrior + " has " + humanWarriorHP + " health and " + skeletonWarrior + " has" + skeletonWarriorHP + " health");
+                        Console.WriteLine(humanWarrior + " attacked " + skeletonWarrior + " and dealt " + humanWarriorDMG + " of damage");
+                        Console.WriteLine(humanWarrior + " has " + humanWarriorHP + " health and " + skeletonWarrior + " has " + skeletonWarriorHP + " health");
 
                         // To continue user has to press a key.
                         Console.WriteLine("Press Enter to continue....");
