914b35ec7b969b0dc49967381977d9d869428657
Mihuy1 <s2000954@edu.bc.fi>
2022-09-20 18:36:43 +0300

    Koodi kokonaan vaihdettu

--- Peli/Program.cs
+++ Peli/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,13 +13,67 @@ namespace Peli
     {
         public static void Main(string[] args)
         {
-            Random rnd = new Random();
+            List<Unit> player_army = new List<Unit>();
+            List<Unit> enemy_army = new List<Unit>();
 
-            Unit unit = new Unit("testi", "testi2", 2, 2, 50, 50);
+            const int HUMANWARRIOR = 1;
+            const int SKELETONWARRIOR = 2;
 
-            unit.Fight();
+            Unit humanWarrior = new Unit("Human Warrior", 5, 10);
+            Unit skeletonWarrior = new Unit("Skeleton Warrior", 5, 15);
 
-           
+            player_army.Add(humanWarrior);
+            enemy_army.Add(skeletonWarrior);
+
+
+            while (true)
+            {
+                if (humanWarrior.hp <= 0)
+                {
+                    Console.WriteLine(skeletonWarrior.name + " won!");
+                    break;
+                }
+                else if (skeletonWarrior.hp <= 0)
+                {
+                    Console.WriteLine(humanWarrior.name + " won!");
+                    break;
+                }
+
+                Console.WriteLine("Choose character: " + "\n" + "1: " + "Human Warrior");
+                int valinta = Convert.ToInt32(Console.ReadLine());
+                
+
+                if (valinta == HUMANWARRIOR)
+                {
+                    Console.WriteLine("You chose: " + humanWarrior.name);;
+                }
+
+
+                FightEnemy();
+
+                Console.WriteLine("Press Enter to Continue....");
+                Console.ReadLine();
+
+
+                FightPlayer(); 
+                
+             
+            }
+
+            void FightEnemy()
+            {
+                skeletonWarrior.hp -= humanWarrior.dmg;
+                Console.WriteLine(humanWarrior.name + " attacked " + skeletonWarrior.name + " and dealt " + humanWarrior.dmg + "\nnow " +
+                skeletonWarrior.name + " has " + skeletonWarrior.hp + " hp");
+            }
+
+            void FightPlayer()
+            {
+                humanWarrior.hp -= skeletonWarrior.dmg;
+                Console.WriteLine(skeletonWarrior.name + " attacked " + humanWarrior.name + "\nNow " + humanWarrior.name + " has "
+                + humanWarrior.hp + " health");
+            }
         }
+
     }
 }
--- Peli/Unit.cs
+++ Peli/Unit.cs
@@ -1,15 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.Cryptography.X509Certificates;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Input;
-
-namespace Peli
+﻿namespace Peli
 {
     public class Unit
     {
+        private const int HUMANWARRIOR = 1;
+        private const int HUMANARCHER = 2;
+        private const int HUMANMAGE = 3;
+
+        private const int SKELETONWARRIOR = 4;
+        private const int SKELETONARCHER = 5;
+        private const int SKELETONMAGE = 6;
+
+        /*
         public string humanWarrior;
         public int humanWarriorHP;
         public int humanWarriorDMG;
@@ -32,55 +33,51 @@ namespace Peli
 
         public string skeletonMage;
         public int skeletonMageHP;
-        public int skeletonMageDMG;
+        public int skeletonMageDMG;*/
 
-        public Unit(string humanWarrior, string skeletonWarrior, int humanWarriorDMG, int skeletonWarriorHP, int humanWarriorHP, int skeletonWarriorDMG)
+        public string name;
+        public int dmg;
+        public int hp;
+
+        public Unit(string name, int dmg, int health)
         {
-            this.humanWarrior = humanWarrior;
-            this.skeletonWarrior = skeletonWarrior;
-
-            this.humanWarriorDMG = humanWarriorDMG;
-            this.skeletonWarriorDMG = skeletonWarriorDMG;
-
-            this.humanWarriorHP = humanWarriorHP;
-            this.skeletonWarriorHP = skeletonWarriorHP;
+            this.name = name;
+            this.dmg = dmg;
+            this.hp = health;
         }
 
         public void Fight()
         {
 
-            humanWarrior = "Human Warrior";
+            /*humanWarrior = "Human Warrior";
             humanArcher = "Human Archer";
             humanMage = "Human Mage";
 
             skeletonWarrior = "Skeleton Warrior";
             skeletonArcher = "Skeleton Archer";
-            skeletonMage = "Skeleton Mage";
+            skeletonMage = "Skeleton Mage";*/
 
-            humanWarriorHP = 100;
+            /*humanWarriorHP = 100;
             humanArcherHP = 60;
-            humanMageHP = 50;
+            humanMageHP = 50;*/
 
-
+            /*
             skeletonWarriorHP = 100;
             skeletonArcherHP = 60;
-            skeletonMageHP = 50;
+            skeletonMageHP = 50;*/
 
             // List armies
+            /*
             Console.WriteLine("Player's army: ");
             Console.WriteLine("1: " + humanWarrior + "\n" + "2: " + humanArcher + "\n" + "3: " + humanMage);
 
             Console.WriteLine("AI's army: ");
-            Console.WriteLine("1: " + skeletonWarrior + "\n" + "2: " + skeletonArcher + "\n" + "3: " + skeletonMage);
+            Console.WriteLine("4: " + skeletonWarrior + "\n" + "5: " + skeletonArcher + "\n" + "6: " + skeletonMage);*/
 
-            // Press enter to start game...
-            Console.WriteLine("To start press enter...");
-            Console.ReadLine();
-
-            Damage();
+           // Damage();
 
         }
-
+        /*
         public void Damage(bool firstPlayerAttacked = false, bool secondPlayerAttacked = false)
         {
 
@@ -96,48 +93,59 @@ namespace Peli
                 {
 
                     // Let the player choose which character attacks and then which enemy it will attack
-                    Console.WriteLine("Give number to choose: ");
+                    Console.WriteLine("Choose character: " + "(1-3)");
                     int player = Convert.ToInt32(Console.ReadLine());
-                    string chosenPlayer;
+                    string chosenPlayer = null;
                     int chosenPlayerDMG;
 
                     // Make the integer equal to the chosen player
-                    if (player == 1)
+                    if (player == HUMANWARRIOR && humanWarriorHP > 0)
                     {
                         chosenPlayer = humanWarrior;
                         chosenPlayerDMG = humanWarriorDMG;
-                        player = humanWarriorHP;
                     }
-                    else if (player == 2)
+
+                    else if (player == HUMANARCHER && humanArcherHP > 0)
                     {
                         chosenPlayer = humanArcher;
                         chosenPlayerDMG = humanArcherDMG;
                         player = humanArcherHP;
                     }
-                    else
+
+                    else if (player == HUMANMAGE && humanMageHP > 0)
                     {
                         chosenPlayer = humanMage;
                         chosenPlayerDMG = humanMageDMG;
                         player = humanMageHP;
                     }
+                    
+                    // Let player choose which target to attack & who attacks
 
-                    Console.WriteLine("Choose target: ");
+                    Console.WriteLine("Choose target: " + "(4-6)");
                     int target = Convert.ToInt32(Console.ReadLine());
-                    string chosenTarget = "s";
+                    string chosenTarget = null;
 
-                    if (target == 1 && skeletonWarriorHP > 0)
+                    if (target == SKELETONWARRIOR && skeletonWarriorHP > 0)
                     {
-                        target = skeletonWarriorHP;
+
+                        target = skeletonWarriorHP; // Target is skeleton warrior
                         chosenTarget = skeletonWarrior;
-                    } else if (target == 2 && skeletonArcherHP > 0)
+                    } 
+
+                    else if (target == SKELETONARCHER && skeletonArcherHP > 0)
                     {
-                        target = skeletonArcherHP;
+                        target = skeletonArcherHP; // target is skeleton archer
                         chosenTarget = skeletonArcher;
-                    } else if (target == 3 && skeletonMageHP > 0)
+                    } 
+                    
+                    else if (target == SKELETONMAGE && skeletonMageHP > 0)
                     {
-                        target = skeletonMageHP;
+                        target = skeletonMageHP; // target is skeleton mage
                         chosenTarget = skeletonMage;
                     }
+
+
+                    // Chosen character attacks enemy
 
                     for (int i = 0; i < 1; i++)
                     {
@@ -146,22 +154,30 @@ namespace Peli
                             Console.WriteLine(chosenPlayer + " died!");
                         }
 
-                        chosenPlayerDMG = rand.Next(5, 20); // Set random damage for chosen character
+                        chosenPlayerDMG = rand.Next(15, 35); // Set random damage for chosen character
 
                         target -= chosenPlayerDMG; // AI takes damage from player
                         firstPlayerAttacked = true; // Player's army has attacked
                         secondPlayerAttacked = false; // AI's army hasn't attacked yet
 
                         // Print dealth damage & health here:
-                        Console.WriteLine(chosenPlayer + " dealt" + chosenPlayerDMG + " damage");
+                        Console.WriteLine(chosenPlayer + " dealt " + chosenPlayerDMG + " damage" + " to " + chosenTarget);
                         Console.WriteLine(chosenTarget + " now has " + target + " health");
 
                         // To continue user has to press a key.
                         Console.WriteLine("Press Enter to continue....");
 
+                        Console.WriteLine("Hwarrior: " + humanWarriorHP + " Harcher: " + humanArcherHP + " Hmage " + humanMageHP + "\n" + " Swarrior: " + skeletonWarriorHP
+                            + " Sarcher: " + skeletonArcherHP + "Smage: " + skeletonMageHP);
+
                         Console.ReadLine();
                     }
                 }
+
+
+
+
+                // AI's turn /////////////////////////////////
 
                 else if (secondPlayerAttacked == false)
                 {
@@ -170,38 +186,47 @@ namespace Peli
                     Console.WriteLine("Press enter to continue...");
                     Console.ReadLine(); // Wait for input to continue
 
-                    int AIHP = rand.Next(1, 3); // virhe!!!!!!!!!!!!!
-                    int AIDMG;
-                    string AICharacter;
+                    int AIChosenCharacter = rand.Next(4, 6); // Random AI character
+                    int AIHP = 0; // HP of the character that was randomly chosen
+                    int AIDMG; // Damage of the character that was randomly chosen
+                    string AICharacter = ""; // Name of the character
 
-                    if (AIHP == 1)
+
+                    
+                    if (AIChosenCharacter == SKELETONWARRIOR && skeletonWarriorHP > 0)
                     {
                         AIHP = skeletonWarriorHP;
                         AICharacter = skeletonWarrior;
                         AIDMG = skeletonWarriorDMG;
                         Console.WriteLine("AI chose " + AICharacter);
-                    } else if (AIHP == 2)
+                    } 
+                    
+                    else if (AIChosenCharacter == SKELETONARCHER && skeletonArcherHP > 0)
                     {
                         AIHP = skeletonArcherHP;
                         AICharacter = skeletonArcher;
                         AIDMG = skeletonArcherDMG;
-                        Console.WriteLine("AI chose" + AICharacter);
-                    } else
+                        Console.WriteLine("AI chose " + AICharacter);
+                    } 
+                    
+                    else if (AIChosenCharacter == SKELETONMAGE && skeletonMageHP > 0)
                     {
                         AIHP = skeletonMageHP;
                         AICharacter = skeletonMage;
                         AIDMG = skeletonMageDMG;
+                        Console.WriteLine("AI chose" + AICharacter);
                     }
+
 
                     int AITargetHP = rand.Next(1, 3); // Randomly choose which character to attack
                     string AITarget; // string of the chosen player character
 
                     // Now make the integer and string equal to the chosen player character
-                    if (AITargetHP == 1)
+                    if (AITargetHP == HUMANWARRIOR)
                     {
                         AITargetHP = humanWarriorHP;
                         AITarget = humanWarrior;
-                    } else if (AITargetHP == 2)
+                    } else if (AITargetHP == HUMANARCHER)
                     {
                         AITargetHP = humanArcherHP;
                         AITarget = humanArcher;
@@ -218,21 +243,19 @@ namespace Peli
 
                     for (int i = 0; i < 1;i++)
                     {
-                        AIDMG = rand.Next(5, 20); // set random damage
+                        AIDMG = rand.Next(50, 60); // set random damage
 
                         AITargetHP -= AIDMG; // First player takes damage from second player.
                         secondPlayerAttacked = true;
                         firstPlayerAttacked = false;
 
-                        Console.WriteLine(AICharacter + " dealt " + AIDMG + " damage");
+                        Console.WriteLine(AICharacter + " dealt " + AIDMG + " damage" + " to " + AITarget);
                         Console.WriteLine(AITarget + " now has " + AITargetHP + " health");
+        */
 
-                        Console.WriteLine("Press Enter to continue....");
-
-                        Console.ReadLine();
-                    }
+                    }/*
                 }
-            }
+            }*/
         }
-    }
-}
+    //}
+//}
