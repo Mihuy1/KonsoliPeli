0e1af70ba74ffab507551b42878d8daa90ba6e8b
Mihuy1 <s2000954@edu.bc.fi>
2022-09-22 14:02:52 +0300

    Changes to Program.cs
    - Added a fighting system with two armies that has 3 characters.

--- Peli/Program.cs
+++ Peli/Program.cs
@@ -16,62 +16,117 @@ namespace Peli
             List<Unit> player_army = new List<Unit>();
             List<Unit> enemy_army = new List<Unit>();
 
+            Random random = new Random();   
+
             const int HUMANWARRIOR = 1;
-            const int SKELETONWARRIOR = 2;
+            const int HUMANARCHER = 2;
+            const int HUMANMAGE = 3;
 
-            Unit humanWarrior = new Unit("Human Warrior", 5, 10);
-            Unit skeletonWarrior = new Unit("Skeleton Warrior", 5, 15);
+            const int SKELETONWARRIOR = 4;
+            const int SKELETONARCHER = 5;
+            const int SKELETONMAGE = 6;
 
-            player_army.Add(humanWarrior);
+            Unit humanWarrior = new Unit("Human Warrior", random.Next(5,10), 15);
+            Unit humanArcher = new Unit("Human Archer", random.Next(5,10), 10);
+            Unit humanMage = new Unit("Human Mage", random.Next(5,10),10);
+
+            Unit skeletonWarrior = new Unit("Skeleton Warrior", random.Next(5,10), 15);
+            Unit skeletonArcher = new Unit("Skeleton Archer", random.Next(5, 10), 10);
+            Unit skeletonMage = new Unit("Skeleton Mage", random.Next(6,10), 10);
+
+            player_army.Add(humanWarrior); // 0 = HUMAWARRIOR - 1
+            player_army.Add(humanArcher);
+            player_army.Add(humanMage);
+            
             enemy_army.Add(skeletonWarrior);
+            enemy_army.Add(skeletonArcher);
+            enemy_army.Add(skeletonMage);
 
 
             while (true)
             {
-                if (humanWarrior.hp <= 0)
-                {
-                    Console.WriteLine(skeletonWarrior.name + " won!");
-                    break;
-                }
-                else if (skeletonWarrior.hp <= 0)
-                {
-                    Console.WriteLine(humanWarrior.name + " won!");
-                    break;
-                }
-
-                Console.WriteLine("Choose character: " + "\n" + "1: " + "Human Warrior");
-                int valinta = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Choose character: " + "\n" + "1: " + humanWarrior.name + "\n2: " + humanArcher.name + "\n3: " + humanMage.name);
+                int attacker = Convert.ToInt32(Console.ReadLine());
                 
-
-                if (valinta == HUMANWARRIOR)
+                // Valitaan koka hyökkää
+                if (attacker == HUMANWARRIOR)
                 {
-                    Console.WriteLine("You chose: " + humanWarrior.name);;
+                    Console.WriteLine("You chose: " + humanWarrior.name);
+                } else if (attacker == HUMANARCHER)
+                {
+                    Console.WriteLine("You chose: " + humanArcher.name);
+                } else if (attacker == HUMANMAGE)
+                {
+                    Console.WriteLine("You chose: " + humanMage.name);
                 }
 
+                // Valitaan vihollinen
+                Console.WriteLine("Choose which enemy to attack:\n4: " + skeletonWarrior.name);
+                int target = Convert.ToInt32(Console.ReadLine());
 
-                FightEnemy();
+                var attackerUnit = player_army[attacker - 1];
+                if (target == SKELETONWARRIOR)
+                {
+                    Console.WriteLine("You chose: " + skeletonWarrior.name);
+                    FightEnemy(attackerUnit, skeletonWarrior);
+                } else if (target == SKELETONARCHER)
+                {
+                    Console.WriteLine("You chose: " + skeletonArcher.name);
+                    FightEnemy(attackerUnit, skeletonArcher);
+                } else if (target == SKELETONMAGE)
+                {
+                    Console.WriteLine("You chose: " + skeletonMage.name);
+                    // Hyökätään vihollista
+                    FightEnemy(attackerUnit, skeletonMage);
+                }
+
+                
 
                 Console.WriteLine("Press Enter to Continue....");
                 Console.ReadLine();
 
+                // Hyökätään pelaajaa
+                FightPlayer();
 
-                FightPlayer(); 
-                
-             
+                Console.WriteLine("Press Enter to continue....");
+                Console.ReadLine();
+
+
             }
 
-            void FightEnemy()
+            void FightEnemy(Unit attacker, Unit target)
             {
-                skeletonWarrior.hp -= humanWarrior.dmg;
-                Console.WriteLine(humanWarrior.name + " attacked " + skeletonWarrior.name + " and dealt " + humanWarrior.dmg + "\nnow " +
-                skeletonWarrior.name + " has " + skeletonWarrior.hp + " hp");
+                target.hp -= attacker.dmg;
+                Console.WriteLine(attacker.name + " attacked " + target.name + " and dealt " + attacker.dmg + "\n" + target.name + " now has " + target.hp + " health");
             }
 
             void FightPlayer()
             {
-                humanWarrior.hp -= skeletonWarrior.dmg;
+                int index = random.Next(player_army.Count);
+
+                if (index == 0)
+                {
+                    humanWarrior.hp -= skeletonWarrior.dmg;
+                    Console.WriteLine(skeletonWarrior.name + " attacked " + humanWarrior.name + " and dealt " + skeletonWarrior.dmg + "\n" + humanWarrior.name + " has "
+                        + humanWarrior.hp + " health");
+
+                } else if (index == 1)
+                {
+                    humanArcher.hp -= skeletonArcher.dmg;
+                    Console.WriteLine(skeletonArcher.name + " attacked " + humanArcher.name + " and dealt " + skeletonArcher.dmg + "\n" + humanArcher.name + " has "
+                        + humanArcher.hp + " health");
+
+                } else if (index == 2)
+                {
+                    humanMage.hp -= skeletonMage.dmg;
+
+                    Console.WriteLine(skeletonMage.name + " attacked " + humanMage.name + " and dealt " + skeletonMage.dmg + "\n" + humanMage.name + " has "
+                        + humanMage.hp + " health");
+                }
+
+                /*humanWarrior.hp -= skeletonWarrior.dmg;
                 Console.WriteLine(skeletonWarrior.name + " attacked " + humanWarrior.name + "\nNow " + humanWarrior.name + " has "
-                + humanWarrior.hp + " health");
+                + humanWarrior.hp + " health");*/
             }
         }
 
