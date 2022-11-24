64b103797894655fa1ca1dd0ca367efc769e0343
Mihuy1 <s2000954@edu.bc.fi>
2022-11-24 14:08:04 +0200

    Added StartWindow script

---
+++ Peli/StartWindow.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Peli
+{
+    internal class StartWindow
+    {
+        public StartWindow()
+        {
+
+        }
+
+        public void Start()
+        {
+            string title = "Humans vs Skeletons";
+
+            Console.SetCursorPosition((Console.WindowWidth - title.Length) / 2, Console.CursorTop);
+            Console.WriteLine(title);
+        }
+    }
+}
